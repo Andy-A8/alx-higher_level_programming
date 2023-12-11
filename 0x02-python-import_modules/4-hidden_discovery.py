95f3a04ddd053a7159ba1d84b612f9eb74791019
Andy-A8 <aaallotey006@st.ug.edu.gh>
2023-12-11 04:20:04 -0800

    update

--- 0x02-python-import_modules/4-hidden_discovery.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-if __name__ == "__main__":
-    """Print all the names defined by the compiled module hidden_4."""
-    import hidden_4
-
-    names = dir(hidden_4)
-    for name in names:
-        if name[:2] != "_":
-            print(name)
