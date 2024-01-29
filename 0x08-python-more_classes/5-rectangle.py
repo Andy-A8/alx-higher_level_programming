c9f03ccdd7981bbe3656197a937b0e7f1756b0ae
Andy-A8 <you@example.com>
2024-01-29 12:33:19 -0800

    A class Rectangle that defines a rectangle by: based on above  - Detect instance deletion

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -80,4 +80,4 @@ class Rectangle:
 
     def __del__(self):
         """Print a message when an instance of Rectangle is deleted"""
-        print("Bye rectanlge...")
+        print("Bye rectangle...")
