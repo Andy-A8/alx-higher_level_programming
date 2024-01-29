92aa9482bc880fe0dbc0fc4dc2f819f2a7231b06
Andy-A8 <you@example.com>
2024-01-29 14:23:05 -0800

    A class Rectangle that defines a rectangle by: based on above  - Compare two rectangles based on area

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -78,8 +78,7 @@ class Rectangle:
 
         if rect_1.area() >= rect_2.area():
             return (rect_1)
-        else:
-            return (rect_2)
+        return (rect_2)
 
     def __str__(self):
         """Return a string with the representation of the Rectangle
