8f62acc76a72c681f37df1a20f36095dd6a289ee
Andy-A8 <aaallotey006@st.ug.edu.gh>
2024-02-12 07:15:28 -0800

    Updated the class Rectangle by adding the public method that assigns an argument to each attribute

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -107,3 +107,31 @@ class Rectangle(Base):
         return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x,
                                                        self.__y, self.__width,
                                                        self.__height)
+
+    def update(self, *args):
+        """Assigns an argument to each attribute
+            Args:
+            *args (int): New attribute values.
+                1st argument - represents id attribute
+                2nd argument - represents width attribute
+                3rd argument - represents height attribute
+                4th argument - represents x attribute
+                5th argument - represents y attribute
+        """
+        if args and len(args) != 0:
+            a = 0
+            for arg in args:
+                if a == 0:
+                    if arg is None:
+                        self.__init__(self.width, self.height, self.x, self.y)
+                    else:
+                        self.id = arg
+                elif a == 1:
+                    self.width = arg
+                elif a == 2:
+                    self.height = arg
+                elif a == 3:
+                    self.x = arg
+                elif a == 4:
+                    self.y = arg
+                a += 1
