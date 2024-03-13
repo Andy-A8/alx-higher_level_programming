e09c1e379c404f09bcb7feb221b421df3b206b1f
Andy-A8 <aaallotey006@st.ug.edu.gh>
2024-03-12 17:29:11 -0700

    A script that removes all records with a score <= 5 in the table second_table of the database hbtn_0c_0 in my MySQL server

---
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -0,0 +1,4 @@
+-- Removes all records with a score <= 5 in the table second_table of the database hbtn_0c_0 in my MySQL server'
+-- Deletes all rows with score less than or equal to 5 in the tale
+
+DELETE FROM `second_table` WHERE `score` <= 5;
--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -10,9 +10,9 @@
 --	id = 4, name = “George”, score = 8
 
 CREATE TABLE IF NOT EXISTS `second_table` (`id` INT, `name` VARCHAR(256), `score` INT);
-INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, "JOHN", 10);
-INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2, "ALEX", 3);
-INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (3, "BOB", 14);
-INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (4, "GEORGE", 8);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, "John", 10);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2, "Alex", 3);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (3, "Bob", 14);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (4, "George", 8);
 
 -- alternatively: insert into second_table (id, name, scre) values (1, JOHN, 10), (2, ALEX, 3), (3, BOB, 14), (4, GEORGE, 8)
