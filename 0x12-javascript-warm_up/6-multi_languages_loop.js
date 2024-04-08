794a86aa13519855ca2c1c25d9fde1ac18ffc2ca
Andy-A8 <aaallotey006@st.ug.edu.gh>
2024-04-08 22:33:33 +0100

    Fixed output error

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const ary = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const ary = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (let i = 0; i < ary.length; ++i) {
   console.log(ary[i]);
