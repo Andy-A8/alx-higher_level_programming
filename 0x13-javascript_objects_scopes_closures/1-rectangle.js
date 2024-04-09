3844dfb4417a1c57cdcb109b69fc8c1906740837
Andy-A8 <aaallotey006@st.ug.edu.gh>
2024-04-09 16:06:37 +0100

    A class Rectangle that defines a rectangle with initialized instance attributes width and height

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+/**
+ * Represents a class Rectangle with initialized instance attributes w and h
+ */
+class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+module.exports = Rectangle;
