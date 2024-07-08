5864970b45bf1cdffebbc3c0d3550b4455943b3a
Andy-A8 <aaallotey006@st.ug.edu.gh>
2024-07-07 17:17:15 -0700

    Updated repository

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-
-const request = require('request');
-const movieid = process.argv[2];
-const url = 'https://swapi-api.alx-tools.com/api/films/' + movieid;
-
-request(url, function (error, reponse, body) {
-  console.log(error || JSON.parse(body).title);
-});
